7356e927e1aaca28afa6ad99abede7fbfd748be4
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-03-31 20:03:34 +0200

    mysql conn init

---
+++ MySQL/Program.cs
@@ -0,0 +1,40 @@
+using MySql.Data.MySqlClient;
+
+namespace MySQL
+{
+    internal static class Program
+    {
+        static MySqlConnection dbConn;
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            DatabaseConnection();
+            Application.Run(new Form1());
+            dbConn?.Close();
+        }
+
+        static void DatabaseConnection()
+        {
+            string connectionString = "server=localhost;" +
+                "user=zoli;" +
+                "password=z;" +
+                "database=gyakorlo_db";
+            dbConn = new MySqlConnection(connectionString);
+            try
+            {
+                dbConn.Open();
+                MessageBox.Show("Connection successful!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}");
+            }
+        }
+    }
+}
