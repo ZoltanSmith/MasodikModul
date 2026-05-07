a3319c3fb03ef8ef9531c0a245d288dfbe7be552
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-05-07 17:59:53 +0200

    tamagocsihoz nevek

--- Tamagocsi/Form1.cs
+++ Tamagocsi/Form1.cs
@@ -9,7 +9,20 @@ namespace Tamagocsi
 
         private void generalasButton_Click(object sender, EventArgs e)
         {
-            List<Allat> allatok;
+            List<Allat> allatok = new();
+            //var allatok = new List<Allat>();
+
+            //Allat allat = new("Bodri", "kutya");
+            //allatok.Add(allat);
+
+            //allatok.Add(new Allat("Bodri", "kutya"));
+            //allatok.Count() // ez a Linq része
+
+            for (int i = 0; i < 12; i++)
+            {
+                allatok.Add(new Allat($"Allat{i}", $"Fajta{i}"));
+            }
+
         }
     }
 }
---
+++ Tamagocsi/Seged.cs
@@ -0,0 +1,35 @@
+﻿namespace Tamagocsi
+{
+    internal static class Seged
+    {
+        static string[] allatNevek = new string[]
+        {
+            "Bodri", "Buksi", "Cirmi", "Lulu", "Béla",
+            "Gombóc", "Fickó", "Mancs", "Pajti", "Süti",
+            "Rex", "Bella", "Daisy", "Max", "Luna",
+            "Bobi", "Cézár", "Mici", "Pamacs", "Tappancs",
+            "Vacak", "Zsemle", "Kormos", "Hófehérke", "Picúr",
+            "Bundás", "Báró", "Molly", "Milo", "Toby"
+        };
+
+        static string[] allatFajtak =
+        [
+            "Aranyretriever", "Német juhászkutya", "Sziámi macska", "Bengáli macska", "Tengerimalac",
+            "Aranyhörcsög", "Afrikai törpesün", "Szakállas agáma", "Nimfapapagáj", "Kanári",
+            "Puli", "Vizsla", "Border Collie", "Perzsa macska", "Ragdoll",
+            "Vörös róka", "Mezei nyúl", "Görény", "Csincsilla", "Axolotl",
+            "Törpehörcsög", "Tacskó", "Mopsz", "Husky", "Maine Coon",
+            "Sarki róka", "Mosómedve", "Láma", "Alpaka", "Kapibara"
+        ];
+
+        static string[] allatEledelek = new string[]
+        {
+            "Száraz kutyatáp", "Konzerves macskaeledel", "Marhahús nyesedék", "Csirkemellfilé", "Lazacolaj",
+            "Főtt tojás", "Almaszeletek", "Sárgarépa", "Madáreleség keverék", "Szárított lisztkukac",
+            "Széna", "Lucerna pellet", "Napraforgómag", "Túró", "Sütőtök püré",
+            "Zöldborsó", "Főtt barna rizs", "Zabpehely", "Kutyakeksz", "Macskamentás jutalomfalat",
+            "Füstölt fül (rágóka)", "Bárányhúsos pástétom", "Pulykanyak", "Spenótlevél", "Banánkarikák",
+            "Kígyóuborka", "Cukkini", "Édesvízi halfilé", "Kókuszolaj", "Joghurt (natúr)"
+        };
+    }
+}
