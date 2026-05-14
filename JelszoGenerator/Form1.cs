13f60259d1f95f1a5cc6de05fbec50b846b36102
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-05-14 18:49:37 +0200

    argon

--- JelszoGenerator/Form1.cs
+++ JelszoGenerator/Form1.cs
@@ -1,4 +1,6 @@
+using Konscious.Security.Cryptography;
 using ReaLTaiizor.Forms;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace JelszoGenerator
@@ -6,6 +8,8 @@ namespace JelszoGenerator
     public partial class Form1 : Form
     {
         Random rnd;
+        String availableCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.-,%:_";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +23,32 @@ namespace JelszoGenerator
 
         private void genButton_Click(object sender, EventArgs e)
         {
+            genereteRandomPassword();
+
+            //vagy
+
+            password.Text = RandomNumberGenerator.GetString(availableCharacters, 6);
+        }
+
+        void genereteRandomPassword()
+        {
             var sb = new StringBuilder();
             for (int i = 0; i < 6; i++)
             {
                 sb.Append((char)rnd.Next(49, 123));
             }
             password.Text = sb.ToString();
+        }
+
+        void genereteArgon2Password()
+        {
+            new Argon2id(Encoding.UTF8.GetBytes("password"))
+            {
+                Salt = Encoding.UTF8.GetBytes("somesalt"),
+                DegreeOfParallelism = 8, // szálak száma generáláskor
+                Iterations = 4,
+                MemorySize = 1024 * 64 // 64 MB
+            }.GetBytes(16);
         }
     }
 }
